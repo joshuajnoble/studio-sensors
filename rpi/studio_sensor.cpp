41fbd5c5b47e1922c0733685a859bbcb0f3c8f51
Joshua Noble <joshuajnoble@gmail.com>
2014-06-13 14:31:58 -0700

    updating dht sensor reading

--- rpi/studio_sensor.cpp
+++ rpi/studio_sensor.cpp
@@ -17,6 +17,9 @@ spiConfig *spiConfig;
 
 #define LIGHT_PIN 15
 
+#define MAXTIMINGS 100
+#define DHT_PIN 12;
+
 using namespace std;
 
 struct atmosphereData {
@@ -90,83 +93,61 @@ int connectAndSend( string hostname )
 }
 
 
-int readDHT(int pin, atmosphereData* aData) {
+int readDHT(atmosphereData* aData) {
   int counter = 0;
   int laststate = HIGH;
   int j=0;
 
-  // Set GPIO pin to output
-  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
-
-  bcm2835_gpio_write(pin, HIGH);
+  bcm2835_gpio_write(dhtPin, HIGH);
   usleep(500000);  // 500 ms
-  bcm2835_gpio_write(pin, LOW);
+  bcm2835_gpio_write(dhtPin, LOW);
   usleep(20000);
 
-  bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(dhtPin, BCM2835_GPIO_FSEL_INPT);
 
   data[0] = data[1] = data[2] = data[3] = data[4] = 0;
 
-  // wait for pin to drop?
-  while (bcm2835_gpio_lev(pin) == 1) {
-    usleep(1);
+  // wait for dhtPin to drop
+  while(bcm2835_gpio_lev(dhtPin) != 0) 
+  {
+    readDHT( 10, &dhtData );
+    std::cout << dhtData.temp << " " dhtData.humidity << std::endl;
+    bcm2835_delay(1000);
   }
 
   // read data!
   for (int i=0; i< MAXTIMINGS; i++) {
     counter = 0;
-    while ( bcm2835_gpio_lev(pin) == laststate) {
-      	counter++;
-      	//nanosleep(1);		// overclocking might change this?
+    while ( bcm2835_gpio_lev(DHT_PIN) == laststate) {
+        counter++;
+        //nanosleep(1);   // overclocking might change this?
         if (counter == 1000) {
-	         break;
+           break;
          }
     }
-    laststate = bcm2835_gpio_lev(pin);
+    laststate = bcm2835_gpio_lev(DHT_PIN);
     if (counter == 1000) break;
     bits[bitidx++] = counter;
 
     if ((i>3) && (i%2 == 0)) {
       // shove each bit into the storage bytes
       data[j/8] <<= 1;
-      if (counter > 200)
+      if (counter > 200) {
         data[j/8] |= 1;
+      }
       j++;
     }
   }
-
-
-#ifdef DEBUG
-  for (int i=3; i<bitidx; i+=2) {
-    printf("bit %d: %d\n", i-3, bits[i]);
-    printf("bit %d: %d (%d)\n", i-2, bits[i+1], bits[i+1] > 200);
-  }
-#endif
-
-  printf("Data (%d): 0x%x 0x%x 0x%x 0x%x 0x%x\n", j, data[0], data[1], data[2], data[3], data[4]);
-
-  if ((j >= 39) && (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) ) 
-  {
-     // yay!
- 
-  	float f, h;
-  	h = data[0] * 256 + data[1];
-  	h /= 10;
-
-  	f = (data[2] & 0x7F)* 256 + data[3];
-    f /= 10.0;
-    if (data[2] & 0x80)  f *= -1;
-    //printf("Temp =  %.1f *C, Hum = %.1f \%\n", f, h);
-    itoa(f, aData->temp);
-    itoa(h, aData->humidity);
-    return 1;
-  }
-
-  return 0;
 }
 
 int main()
 {
+
+  // start up dht sensor
+  // Set GPIO dhtPin to output
+  bcm2835_gpio_fsel(DHT_PIN, BCM2835_GPIO_FSEL_OUTP);
+
+  // startup spi
   spiConfig->mode = SPI_MODE_0;
   spiConfig->bitsPerWord = 8;
   spiConfig->speed = 1000000;
