47f9a26ca232d245123969c95e6a0f0f160be9fb
joshuajnoble <joshuajnoble@gmail.com>
2014-10-15 21:45:45 +0000

    adding sound averaging

--- rpiPy/studio_sensors.py
+++ rpiPy/studio_sensors.py
@@ -14,31 +14,34 @@ from subprocess import call
 TSL235_pin = 7
 DHT22_pin = 17
 sensor_id = -1
+
 def bitstring(n):
     s = bin(n)[2:]
     return '0'*(8-len(s)) + s
 
 def readADC(adc_channel=0, spi_channel=0):
-    conn = spidev.SpiDev(0, spi_channel)
-    conn.max_speed_hz = 1200000 # 1.2 MHz
-    cmd = 128
-    if adc_channel:
-        cmd += 32
-    reply_bytes = conn.xfer2([cmd, 0])
-    reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
-    reply = reply_bitstring[5:15]
-    return int(reply, 2) / 2**10
-
-#def read_light(pi):
-#    NUM_CYCLES = 10
-#    start = time.time()
-#    for impulse_count in range(NUM_CYCLES):
-#    	pi.wait_for_edge(TSL235_pin, pigpio.FALLING_EDGE)
-#        duration = time.time() - start      #seconds to run for loop
-#    
-#    return NUM_CYCLES / duration   #in Hz
+	conn = spidev.SpiDev(0, spi_channel)
+	conn.max_speed_hz = 500000 # 1.2 MHz
+	conn.mode = 0
+	cmd = 192
+	if adc_channel != 0:
+		cmd += 32
+	reply_bytes = conn.xfer2([cmd, 0])
+	reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
+	reply = reply_bitstring[5:15]
+	return int(reply, 2)
 
 
+#def readADC(adc_channel=0, spi_channel=0):
+#    conn = spidev.SpiDev(0, spi_channel)
+#    conn.max_speed_hz = 1200000 # 1.2 MHz
+#    cmd = 128
+#    if adc_channel:
+#        cmd += 32
+#    reply_bytes = conn.xfer2([cmd, 0])
+#    reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
+#    reply = reply_bitstring[5:15]
+#    return int(reply, 2) / 2**10
 
 ####################################################################################################
 # now begin the actual loop
@@ -46,22 +49,20 @@ def readADC(adc_channel=0, spi_channel=0):
 
 if __name__ == "__main__":
 
-    pir_pin = 18
-    pi = pigpio.pi()
-    pi.set_mode(pir_pin, pigpio.INPUT)         # activate input
+	pir_pin = 18
+	pi = pigpio.pi()
+	pi.set_mode(pir_pin, pigpio.INPUT)         # activate input
 
-    # Sensor should be set to Adafruit_DHT.DHT11,
-    # Adafruit_DHT22, or Adafruit_AM2302.
-    sensor = Adafruit_DHT.DHT22
+	sensor = Adafruit_DHT.DHT22
 
-    last_send = time.time()
-
-    pir_triggered = False
-    has_id = False
+	last_send = time.time()
+	last_sound_sample = last_send
+	pir_triggered = False
+	has_id = False
 
 	# if you have an ID file then you don't need to do this
-        
-    if os.path.isfile('studio_sensor_id') == False:
+
+	if os.path.isfile('studio_sensor_id') == False:
 		# maybe this is wonky?
 		while has_id == False:
 			request = urllib2.Request('http://162.242.237.33:3000/get_id')
@@ -83,75 +84,83 @@ if __name__ == "__main__":
 				 sleep(10)
 				 call(["ifup", "wlan0"])
 				 print('URLError = ' + str(e.reason))
-#			except urllib2.HTTPException, e:
-#				 call(["ifdown", "wlan0"])
-#				 sleep(10)
-#				 call(["ifup", "wlan0"])
-#				 print('HTTPException')
+			except urllib2.HTTPException, e:
+				 call(["ifdown", "wlan0"])
+				 sleep(10)
+				 call(["ifup", "wlan0"])
+				 print('HTTPException')
 			except Exception:
 				 import traceback
 				 print('generic exception: ' + traceback.format_exc())
 				
 			sleep(10)
 
-    if os.path.isfile('studo_sensor_id') == True:
-        f = open('studio_sensor_id', 'r')
-        sensor_id = f.read()        
-        f.close()    
+	if os.path.isfile('studio_sensor_id') == True:
+		f = open('studio_sensor_id', 'r')
+		sensor_id = f.read()        
+		f.close()    
 
-    while 1:
-    	
-    	sleep(0.1)
-    	
-    	if pi.read(pir_pin):
-            pir_triggered = True
-        else:
-            pir_triggered = False
+	sound_values = [0] * 13
+	sound_index = 0
 
-        if (time.time() - last_send) > 60:
+	while 1:
+		sleep(0.1)
+		if pi.read(pir_pin) == False:
+			pir_triggered = True
+		else:
+			pir_triggered = False
 
-            send = "i=" + str(sensor_id)
-            if pir_triggered:
-                send += "&m=1"
-            else:
-                send += "&m=0"
+		if( time.time() - last_sound_sample) > 5:
+			sound_values[sound_index] = readADC()
+			sound_index += 1
+			last_sound_sample = time.time()
 
-            light_value = readADC(1) #light is now just on MCP3002, tsl235 can stuff it
-            send += "&l=" + str(light_value)
 
-            sound_value = readADC()
-            send += "&s=" + str(sound_value)
+		if (time.time() - last_send) > 60:
+			print( " sending ")
+			send = "i=" + str(sensor_id)
+			if pir_triggered:
+				send += "&m=1"
+			else:
+				send += "&m=0"
+			light_value = readADC(1) #light is now just on MCP3002, tsl235 can stuff it
+			send += "&l=" + str(light_value)
+			
+			sound_sum = 0
+			for val in sound_values:
+				sound_sum += val
 
-            humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT22_pin)
+			send += "&s=" + str(sound_sum / sound_index)
+			# all done with sounds until next send
+			sound_index = 0
+			humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT22_pin)
+			print( str(temperature) + " " + str(humidity))
+			send += "&t=" + str(temperature) + "&h=" + str(humidity)
 
-            if humidity is not None and temperature is not None:
-            	send += "&t=" + str(temperature) + "&h=" + str(humidity)
-            else:
-            	humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT22_pin)
-                send += "&t=" + str(temperature) + "&h=" + str(humidity)
+			print( send )
 
-            # we're going to want this to allow us to pull this out of the
-            # config file so it can be set from the desktop I'd think
-            request = urllib2.Request('http://162.242.237.33:3000/?'+send)
-            try: 
-                response = urllib2.urlopen(request)
-            except urllib2.HTTPError, e:
-                print('HTTPError = ' + str(e.code))
-                call(["ifdown", "wlan0"])
-                sleep(10)
-                call(["ifup", "wlan0"])
-                print( " http error " )
-            except urllib2.URLError, e:
-                call(["ifdown", "wlan0"])
-                sleep(10)
-                call(["ifup", "wlan0"])
-                print('URLError = ' + str(e.reason))
-#            except httplib.HTTPException, e:
-#                call(["ifdown", "wlan0"])
-#                sleep(10)
-#                call(["ifup", "wlan0"])
-#                print('HTTPException')
-            except Exception:
+			# we're going to want this to allow us to pull this out of the
+			# config file so it can be set from the desktop I'd think
+			request = urllib2.Request('http://162.242.237.33:3000/?'+send)
+			try: 
+				response = urllib2.urlopen(request)
+			except urllib2.HTTPError, e:
+				print('HTTPError = ' + str(e.code))
+				call(["ifdown", "wlan0"])
+				sleep(10)
+				call(["ifup", "wlan0"])
+				print( " http error " )
+			except urllib2.URLError, e:
+				call(["ifdown", "wlan0"])
+				sleep(10)
+				call(["ifup", "wlan0"])
+				print('URLError = ' + str(e.reason))
+			except urllib2.HTTPException, e:
+				call(["ifdown", "wlan0"])
+				sleep(10)
+				call(["ifup", "wlan0"])
+				print('HTTPException')
+			except Exception:
 				import traceback
 				print('generic exception: ' + traceback.format_exc())
 				call(["ifdown", "wlan0"])
@@ -159,10 +168,5 @@ if __name__ == "__main__":
 				call(["ifup", "wlan0"])
 				print( " http error " )
 
-
- 
-            print send
-            urllib2.close()
-            last_send = time.time()
-
+			last_send = time.time()
 # end
