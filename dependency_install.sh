ffd34af9b43675ecdd005e7c7486b964270106cc
Joshua Noble <joshuajnoble@gmail.com>
2014-11-07 15:17:13 -0800

    massive cleanup

--- dependency_install.sh
+++
@@ -1,5 +0,0 @@
-apt-get install -y gzip git-core curl python-software-properties python g++ make libssl-dev pkg-config build-essential
-add-apt-repository ppa:chris-lea/node.js
-echo "deb http://us.archive.ubuntu.com/ubuntu/ precise universe" >> /etc/apt/sources.list
-apt-get update
-apt-get install -y nodejs
